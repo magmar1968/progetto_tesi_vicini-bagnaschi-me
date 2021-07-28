888f71efaf4bf85cc64db5ecca234a07fdca1b34
Lorenzo Magnoni <lorenzomagnoni@pcteor1.mi.infn.it>
2021-07-28 17:03:33 +0200

    update bash script

--- script_slurm.sh
+++ script_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MYDIR=/home/lorenzomagnoni/progetto_tesi/POWHEG/W_ew-BMNNP/
+MYDIR=/home/lorenzomagnoni/progetto_tesi/POWHEG-BOX/W_ew-BMNNP/
 
 dirpdfs=(NNPDF31_nlo_as_0118)
-inputfile=(pwhg.input)
+inputfile=(powheg.input)
 
 risultati=risultati1
 mkdir -p $risultati
@@ -11,15 +11,18 @@ mkdir -p $risultati
 echo "#!/bin/bash
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=500M
-#SBATCH --time=2-0:0
+#SBATCH --time=1-0:0
 
 cd "'$SLURM_TMPDIR'"
-cp ${MYDIR}pwhg_main
-cp ${MYDIR}/test-el/$inputfile
+cp ${MYDIR}pwhg_main .
+cp ${MYDIR}test-el/$inputfile .
+cp ${MYDIR}test-el/pwgseeds.dat .
 export LHAPATH=${HOME}/local/share/LHAPDF/
-./pwhg_main < ${inputfile} >out0 
-cp out0 ${MYDIR}${risultati}/" >a.sh
+./pwhg_main  < $inputfile
+ls -la
+cp ./* ${MYDIR}${risultati}/" > a.sh
 
+cat a.sh
 sbatch --partition=general a.sh
 
 sleep 0.5s
