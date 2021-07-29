0c60f02699b7a118948a62cf7dde46c4b38c3725
Lorenzo Magnoni <lorenzomagnoni@pcteor1.mi.infn.it>
2021-07-29 11:06:28 +0200

    tiny update still not working

--- script_slurm_array.sh
+++ script_slurm_array.sh
@@ -5,21 +5,25 @@ MYDIR=/home/lorenzomagnoni/progetto_tesi/
 dirpdfs=(NNPDF31_nlo_as_0118)
 inputfile=(powheg.input)
 
-risultati=risultati1
-mkdir -p $risultati
+
+
 
 echo "#!/bin/bash
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=500M
-#SBATCH --time=1-0:0
+#SBATCH --time=20-0:0
+#SBATCH --array=1-3
 
+ID="'$SLURM_ARRAY_TASK_ID'"
 cd "'$SLURM_TMPDIR'"
 cp ${MYDIR}pwhg_main .
 cp ${MYDIR}$inputfile .
 cp ${MYDIR}pwgseeds.dat .
 export LHAPATH=${HOME}/local/share/LHAPDF/
-./pwhg_main  < $inputfile
+echo $ID | ./pwhg_main  < $inputfile
 ls -la
+risultati="ris_$ID"
+mkdir -p ${risultati}/
 cp ./* ${MYDIR}${risultati}/" > a.sh
 
 cat a.sh
