54888f8b929ef561da2a42229c13b91a451a8d05
Lorenzo Magnoni <lorenzomagnoni@pcteor1.mi.infn.it>
2021-07-29 16:19:18 +0200

    file bash definitivo e gen_seeds

--- script_slurm_array.sh
+++ script_slurm_array.sh
@@ -5,26 +5,26 @@ MYDIR=/home/lorenzomagnoni/progetto_tesi/
 dirpdfs=(NNPDF31_nlo_as_0118)
 inputfile=(powheg.input)
 
-
-
-
 echo "#!/bin/bash
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=500M
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=1G
 #SBATCH --time=20-0:0
 #SBATCH --array=1-3
 
-ID="'$SLURM_ARRAY_TASK_ID'"
-cd "'$SLURM_TMPDIR'"
+ID=\$SLURM_ARRAY_TASK_ID
+RISULTATI=${MYDIR}/risultati/ris_\$ID/
+mkdir -p \$RISULTATI
+cd \$SLURM_TMPDIR
+
+echo task id: \$ID
 cp ${MYDIR}pwhg_main .
 cp ${MYDIR}$inputfile .
 cp ${MYDIR}pwgseeds.dat .
 export LHAPATH=${HOME}/local/share/LHAPDF/
-echo $ID | ./pwhg_main  < $inputfile
-ls -la
-risultati="ris_$ID"
-mkdir -p ${risultati}/
-cp ./* ${MYDIR}${risultati}/" > a.sh
+
+echo \$ID | ./pwhg_main > powheg-\$ID.output
+cp powheg-\$ID.output \$RISULTATI
+cp ./* \$RISULTATI" > a.sh
 
 cat a.sh
 sbatch --partition=general a.sh
