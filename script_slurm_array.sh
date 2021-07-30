0b593a6e6a83c16f25bcc68849bc75f607f823d9
Lorenzo Magnoni <lorenzomagnoni@pcteor1.mi.infn.it>
2021-07-30 16:38:17 +0200

    update array

--- script_slurm_array.sh
+++ script_slurm_array.sh
@@ -9,10 +9,13 @@ echo "#!/bin/bash
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=20-0:0
-#SBATCH --array=1-3
+#SBATCH --array=1-4
+
+SECONDS=0
 
 ID=\$SLURM_ARRAY_TASK_ID
 RISULTATI=${MYDIR}/risultati/ris_\$ID/
+FILEOUT=out-\$ID.tar.gz
 mkdir -p \$RISULTATI
 cd \$SLURM_TMPDIR
 
@@ -23,10 +26,20 @@ cp ${MYDIR}pwgseeds.dat .
 export LHAPATH=${HOME}/local/share/LHAPDF/
 
 echo \$ID | ./pwhg_main > powheg-\$ID.output
-cp powheg-\$ID.output \$RISULTATI
-cp ./* \$RISULTATI" > a.sh
+
+duration=\$SECONDS
+echo \$duration
+echo minutes: ((\$duration / 60))
+
+#copy from slurm directory
+tar cvzf \$FILEOUT ./*
+cp \$FILEOUT \$RISULTATI" > a.sh
 
 cat a.sh
 sbatch --partition=general a.sh
 
 sleep 0.5s
+
+mkdir -p file_log/
+mv slurm-* ./file_log/
+
